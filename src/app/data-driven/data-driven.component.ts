e8556bd8d2490a2520d8607c851d2c9a33bee5d1
f.taleb <farzanehtaleb11@gmail.com>
2017-08-02 10:14:25 +0430

    110 - 114 Data Driven Form

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
   templateUrl: './data-driven.component.html',
   styleUrls: ['./data-driven.component.css']
 })
-export class DataDrivenComponent implements OnInit {
+export class DataDrivenComponent {
 
-  constructor() { }
+  myForm: FormGroup ;
 
-  ngOnInit() {
+  constructor() {
+    this.myForm = new FormGroup({
+      'username': new FormControl() ,
+      'password': new FormControl(),
+      'email': new FormControl()
+    });
+
   }
+  onSubmit = function () {
+console.log(this.myForm) ;
+  };
+
+
 
 }
